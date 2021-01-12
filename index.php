82be316d1b6d5a6afb387b9427164b85c3c57ba1
NateAgcaoili <75859423+NateAgcaoili@users.noreply.github.com>
2021-01-12 13:58:11 -0500

    Learned string and number functions

--- index.php
+++ index.php
@@ -7,12 +7,33 @@
 </head>
 <body>
     <?php
-        $characterName = "John";
-        $characterAge = 35;
-        echo "<br>There once was a man named $characterName</br>";
-        echo "<br>He was $characterAge years old</br>";
-        echo "<br>He really liked the name $characterName</br>";
-        echo "<br>But didn't like being $characterAge</br>";
+        //string practice
+        $phrase = "Let's GOOOO";
+        echo strtolower($phrase);
+        echo strtoupper($phrase);
+        echo strlen($phrase);
+        echo $phrase[0];
+        echo str_replace("GOOOO", "not.", $phrase);
+        echo substr($phrase, 6, 5);
+        //number practice
+        echo 5 + 9;
+        $demoNum = 1;
+        $demoNum++;
+        echo $demoNum;
+        $demoNum += 10;
+        echo $demoNum;
+        echo abs(-100);
+        echo pow(2, 4);
+        echo sqrt(16);
+        echo pow(16, 0.5);
+        echo max(2, 10);
+        echo min(2, 10);
+        echo round(1.5);
+        echo round(1.44);
+        echo ceil(1.5);
+        echo ceil(1.44);
+        echo floor(1.5);
+        echo floor(1.44);
     ?>
 </body>
 </html>
