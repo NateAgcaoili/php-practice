ba70f95fcaa4c9cacb658b29db0cceb160669fe3
NateAgcaoili <75859423+NateAgcaoili@users.noreply.github.com>
2021-01-13 01:39:38 -0500

    Learned associative arrays

--- index.php
+++ index.php
@@ -6,11 +6,14 @@
     <title>Document</title>
 </head>
 <body>
-    
-    <?php 
-        $friends = array("Arielle", "Nuggie", "Lez", "Squirt", "Thomas");
-        echo $friends[0];
-        echo count($friends);
+    <form action="index.php" method="post">
+        <input type="text" name="student"><br>
+        <input type="submit">
+    </form>
+    <?php
+        $studentGrade = $_POST["student"];
+        $grades = array("Jim"=>"A+", "Pam"=>"B-", "Oscar"=>"C+");
+        echo $grades[$studentGrade];
     ?>
 </body>
 </html>
