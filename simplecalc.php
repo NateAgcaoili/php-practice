3a1459a106cf7723028180b5b0c99fcf76217180
NateAgcaoili <75859423+NateAgcaoili@users.noreply.github.com>
2021-01-13 17:31:43 -0500

    Update simplecalc.php

--- simplecalc.php
+++ simplecalc.php
@@ -6,12 +6,28 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="simplecalc.php" method="get">
-        <input type="number" name="num1">
-        <br>
-        <input type="number" name="num2">
+    <form action="simplecalc.php" method="post">
+        First Num: <input type="number" name="num1"> <br>
+        Operator: <input type="textbox" name="op"> <br>
+        Second Num: <input type="number" name="num2"> <br>
         <input type="submit">
     </form>
-    Answer: <?php echo $_GET["num1"] + $_GET["num2"]?>
+    <?php
+        $num1 = $_POST["num1"];
+        $num2 = $_POST["num2"];
+        $op = $_POST["op"];
+
+        if ($op == "+") {
+            echo $num1 + $num2;
+        } elseif ($op == "-") {
+            echo $num1 - $num2;
+        } elseif ($op == "*") {
+            echo $num1 * $num2;
+        } elseif ($op == "/") {
+            echo $num1 / $num2;
+        } else {
+            echo "Please enter a valid operator (+, -, *, /)";
+        }
+    ?>
 </body>
 </html>
